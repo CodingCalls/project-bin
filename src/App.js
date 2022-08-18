48d55343e668d3cb98c9c15d463764d3d09e286b
AdityaNathK <aditya.nath@aligent.com.au>
2022-08-18 16:23:45 +0930

    Add TestComponent and absolute imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import classes from  "./app.module.scss";
+import TestComponent from 'components/TestComponent';
+import classes from './app.module.scss';
 
 const App = () => {
     return (
         <div className={classes.root}>
             <h1>React Template</h1>
+            <TestComponent />
         </div>
     );
 };
---
+++ src/components/TestComponent/TestComponent.jsx
@@ -0,0 +1,7 @@
+import classes from './TestComponent.module.scss';
+
+const TestComponent = () => {
+    return <div className={classes.root}>TestComponent</div>;
+};
+
+export default TestComponent;
