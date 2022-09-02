f8bd5041de0ebf56f30b3cd288b3e6a658ab66ac
AdityaNathK <aditya.nath@aligent.com.au>
2022-09-02 16:32:40 +0930

    RT-04 Add classnames package and button component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
-import PageNotFoundError from "components/PageNotFoundError";
+import Button from "components/Button";
 import classes from "./App.module.scss";
 
 const App = () => {
     return (
         <>
-            <PageNotFoundError />
+            <Button />
         </>
     );
 };
